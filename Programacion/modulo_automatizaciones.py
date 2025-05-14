8267ab1c440fcdaa4f93055f00b1123a6db65e99
Paubs13 <paulo.alessandro05@gmail.com>
2025-05-14 20:34:28 -0300

    Se realizo el boceto de las dos funciones, falta quitar los inputs y compatibilizar con el resto del codigo (que tambien hay que pulir bastane)

--- Programacion/modulo_automatizaciones.py
+++ Programacion/modulo_automatizaciones.py
@@ -1,2 +1,36 @@
-# AGREGAR AUTOMATIZACION (CUSTOM)
-# ELIMINAR AUTOMATIZACION (CUSTOM)
+#agregar en main.py automatizaciones = [diccionario], tpos_automatizaciones = [diccionario]
+#eliminar inputs y ponerlos en main
+automatizaciones = {
+    
+}
+tipos_automatizaciones = {'luz': {"tipo_condicion" : "luminosidad"},
+    'temperatura': {"tipo_condicion" :"grados celsius"},
+    'cortinas y ventanas' : {"tipo_condicion" : ['luminosidad', 'grados celsius']}
+}
+
+def añadir_automatizacion():
+    nombre = input("Ingrese el nombre de la automatizacion")
+    print("Elija el tipo de automatizacion: \n", *tipos_automatizaciones, sep =', ')
+    tipo = input("==> ")
+
+    if tipo in tipos_automatizaciones:
+        automatizaciones[nombre] = {"tipo" : tipo}
+
+        print('Ingrese la condicion de comienzo en', *tipos_automatizaciones[tipo]["tipo_condicion"], sep =', ')
+        condicion_comienzo = input(' ==> ')
+        automatizaciones[nombre]["condicion_comienzo"] = condicion_comienzo
+
+        print('Ingrese la condicion de comienzo en', *tipos_automatizaciones[tipo]["tipo_condicion"], sep =', ')
+        condicion_corte = input(' ==> ')
+        automatizaciones[nombre]["condicion_corte"] = condicion_corte
+
+    else:
+        print("Tipo no listado")
+
+def eliminar_automatizacion():
+    eleccion = input("Seguro que desea eliminar la automatizacion? s/n ")
+    if eleccion != "s" and eleccion != "n":
+        print("Eleccion invalida")
+    elif eleccion == "s":
+        automatizaciones.clear()
+eliminar_automatizacion()
