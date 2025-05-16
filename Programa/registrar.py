744d9568549e6b01a31146be7443cd1a9396c417
Jeremias <jerearrieta2254@gmail.com>
2025-05-16 18:42:42 -0300

    Eliminacion de carpeta extra

--- Programa/registrar.py
+++
@@ -1,9 +0,0 @@
-def registrar_usuario(nombre, contraseña, datos_usuario):
-    usuario = {"Usuario" : nombre, "Contraseña" : contraseña}
-    datos_usuario.append(usuario)
-
-
-
-def agregar_dispositivos(nombre_dispositivo, seleccion_tipo, datos_dispositivos):
-    dispositivo = {"Nombre" : nombre_dispositivo, "Tipo" : seleccion_tipo}
-    datos_dispositivos.append(dispositivo)
